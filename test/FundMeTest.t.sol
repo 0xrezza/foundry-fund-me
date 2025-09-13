1b897acf083e20068d9da7435f35b9bc24071bc7
0xrezza <rezaghoreyshi.contact@gmail.com>
2025-09-13 20:15:16 +0330

    add test for data feed version check using fork url

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -6,17 +6,21 @@ import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+
     function setUp() external {
-         fundMe = new FundMe();
+        fundMe = new FundMe();
     }
 
     function testMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
-
     }
 
     function testOwnerIsMsgSender() public view {
-       
         assertEq(fundMe.i_owner(), address(this));
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
